b56007e6a5bc1b9cbe51dc78eece1bbc76792cdb
zenzhu <zic.zhu@gmail.com>
2016-04-29 14:10:23 +0800

    Clear comments

--- test/macaca-electron.test.js
+++ test/macaca-electron.test.js
@@ -1,22 +1,9 @@
-/* ================================================================
- * macaca-electron by xdf(xudafeng[at]126.com)
- *
- * first created at : Tue Mar 29 2016 09:11:49 GMT+0800 (CST)
- *
- * ================================================================
- * Copyright  xdf
- *
- * Licensed under the MIT License
- * You may not use this file except in compliance with the License.
- *
- * ================================================================ */
-
 'use strict';
 
 var co = require('co');
 var path = require('path');
 
-var Electron = require('..');
+var Electron = require('../lib/macaca-electron');
 var _ = require('../lib/helper');
 
 describe('unit testing', function() {
