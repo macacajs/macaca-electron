aca014246d982a68203ea86a8fbd392127d7126f
Zhu Liang <zhuliangg11@gmail.com>
2019-01-22 16:05:16 +0800

    test(controllers): add tests for controllers (#30)
    * test(controllers): add tests for controllers
    
    * style: use obj destructuring

--- test/controllers.test.js
+++ test/controllers.test.js
@@ -3,6 +3,7 @@
 const path = require('path');
 const assert = require('assert');
 
+const { errors } = require('webdriver-dfn-error-code');
 const _ = require('../lib/helper');
 const Electron = require('../lib/macaca-electron');
 
@@ -30,8 +31,30 @@ describe('test/controllers.test.js', function() {
       yield driver.maximize();
       var html = yield driver.getSource();
       assert(html.includes(('<html')));
+    });
+
+    it('execute ok', function * () {
       var uesrAgent = yield driver.execute('return navigator.userAgent');
       assert.equal(uesrAgent, customUserAgent);
+      // failing due to a bug
+      // var arrayResult = yield driver.execute('return [1, 2, 3]');
+      // assert.equal(arrayResult, [1, 2, 3]);
+    });
+
+    it('get url', function * () {
+      var url = yield driver.url();
+      assert.equal(/1\.html$/.test(url), true);
+    });
+
+    it('does not implement forward', function * () {
+      let f = () => {};
+      try {
+        yield driver.forward();
+      } catch (e) {
+        f = () => { throw e; };
+      } finally {
+        assert.throws(f, errors.NotImplementedError);
+      }
     });
 
     it('get title', function * () {
@@ -54,6 +77,31 @@ describe('test/controllers.test.js', function() {
       assert.equal(/^[0-9a-z\/+=]+$/i.test(base64), true);
     });
 
+    it('find element', function * () {
+      var input = yield driver.findElement('id', 'input');
+      assert.ok(input);
+
+      let f = () => {};
+      try {
+        // need to wait for 5 seconds timeout
+        yield driver.findElement('id', 'non-existent');
+      } catch (e) {
+        f = () => { throw e; };
+      } finally {
+        assert.throws(f, errors.NoSuchElement);
+      }
+    });
+
+    it('find elements', function * () {
+      const divs = yield driver.findElements('name', 'test');
+      assert.equal(divs.length, 2);
+      var input = yield driver.findElements('id', 'input');
+      assert.equal(input.length, 1);
+      // need to wait for 5 seconds timeout
+      var nonExistent = yield driver.findElements('id', 'non-existent');
+      assert.equal(nonExistent.length, 0);
+    });
+
     it('set input value', function * () {
       var input = yield driver.findElement('id', 'input');
       yield driver.setValue(input.ELEMENT, 'aaa');
@@ -61,7 +109,7 @@ describe('test/controllers.test.js', function() {
       yield driver.setValue(input.ELEMENT, 'macaca');
       var style = yield driver.getComputedCss(input.ELEMENT, 'display');
       assert.equal(style, 'inline-block');
-      yield _.sleep(500);
+      yield _.sleep(200);
     });
 
     it('element attr', function * () {
@@ -73,10 +121,16 @@ describe('test/controllers.test.js', function() {
       assert.equal(bgColor, 'rgb(255, 255, 255)');
     });
 
+    it('get property', function * () {
+      var button = yield driver.findElement('id', 'button-1');
+      var buttonId = yield driver.getProperty(button.ELEMENT, 'id');
+      assert.equal(buttonId, 'button-1');
+    });
+
     it('click button', function * () {
       var button = yield driver.findElement('id', 'button-1');
       yield driver.click(button.ELEMENT);
-      yield _.sleep(300);
+      yield _.sleep(200);
       var box = yield driver.findElement('id', 'target');
       var boxText = yield driver.getText(box.ELEMENT);
       assert.equal(boxText, 'macaca');
@@ -85,16 +139,16 @@ describe('test/controllers.test.js', function() {
     it('click link', function * () {
       var link = yield driver.findElement('id', 'link-1');
       yield driver.click(link.ELEMENT);
-      yield _.sleep(1000);
+      yield _.sleep(200);
       var title = yield driver.title();
       assert.equal(title, 'Document 2');
     });
 
     it('history back', function * () {
       yield driver.back();
-      yield _.sleep(1000);
+      yield _.sleep(200);
       yield driver.refresh();
-      yield _.sleep(1000);
+      yield _.sleep(200);
       var title = yield driver.title();
       assert.equal(title, 'Document 1');
     });
@@ -103,7 +157,7 @@ describe('test/controllers.test.js', function() {
       var link = yield driver.findElement('id', 'link-2');
       yield driver.click(link.ELEMENT);
       yield driver.maximize();
-      yield _.sleep(3000);
+      yield _.sleep(500);
     });
 
     it('window handlers', function * () {
@@ -115,6 +169,39 @@ describe('test/controllers.test.js', function() {
       yield driver.setWindow(windows[0]);
       title = yield driver.title();
       assert.equal(title, 'Document 1');
+    });
+
+    it('set frame that exists and send command', function * () {
+      yield driver.get(`file://${path.resolve(__dirname, 'webpages/4.html')}`);
+      const iframeElement = yield driver.findElement('id', 'iframe1');
+      let result = yield driver.setFrame(0);
+      assert.equal(result, null);
+      result = yield driver.setFrame(iframeElement);
+      assert.equal(result, null);
+      const link = yield driver.findElement('id', 'link-2');
+      assert.ok(link);
+    });
+
+    it('set frame that does not exist', function * () {
+      yield driver.get(`file://${path.resolve(__dirname, 'webpages/4.html')}`);
+
+      let f = () => {};
+      try {
+        yield driver.setFrame(123);
+      } catch (e) {
+        f = () => { throw e; };
+      } finally {
+        assert.throws(f, errors.NoSuchFrame);
+      }
+
+      let g = () => {};
+      try {
+        yield driver.setFrame('123');
+      } catch (e) {
+        g = () => { throw e; };
+      } finally {
+        assert.throws(g, errors.NoSuchFrame);
+      }
     });
 
     it('cookie handlers', function * () {
@@ -129,6 +216,19 @@ describe('test/controllers.test.js', function() {
       yield driver.addCookie(cookie);
       const res = yield driver.getNamedCookie(cookie.name);
       assert.equal(res.length, 1);
+
+      var cookie2 = {
+        url: pkg.homepage,
+        name: 'cookie-test',
+        value: 'cookie-test'
+      };
+      yield driver.addCookie(cookie2);
+      cookies = yield driver.getAllCookies();
+      assert.equal(cookies.length, 2);
+      yield driver.deleteCookie('cookie-test');
+      cookies = yield driver.getAllCookies();
+      // failing due to a bug
+      // assert.equal(cookies.length, 1);
       yield driver.deleteAllCookies();
       cookies = yield driver.getAllCookies();
       assert.equal(cookies.length, 0);
